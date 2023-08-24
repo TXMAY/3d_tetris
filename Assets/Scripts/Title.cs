b3445e7a80d47315bc6b144745313517e4c0afb0
TXMAY <47449096+TXMAY@users.noreply.github.com>
2023-08-24 09:54:59 +0900

    Add Main Menu (without logo)

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string nextScene;
+
+    // 게임 실행
+    public void StartGame()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    // 게임 종료
+    public void ExitGame()
+    {
+        // 게임 종료(에디터)
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        // 게임 종료(프로그램)
+#else
+        Application.Quit();
+#endif
+    }
+}
